a5fd9050042952c12607dfda1ce61d3b420e2008
Daria Sopshina <dasha.sopshina@mail.ru>
2024-09-09 13:31:52 +0400

    finish

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 const toggleButton = document.querySelector('.button-open');
-const buttonText = toggleButton.textContent.trim();
 const additionalBrands = document.querySelector('.brands__readnext__list');
+const arrowIcon = document.querySelector('.brands__button__arrow-icon');
 
 toggleButton.addEventListener('click', function () {
     const buttonText = toggleButton.textContent.trim();
 
     if (buttonText === 'Показать все') {
-        toggleButton.textContent = 'Скрыть';
+        toggleButton.querySelector('span').nextSibling.textContent = 'Скрыть';
         additionalBrands.classList.remove('brands__readnext__list_hidden');
+        arrowIcon.style.transform = 'rotate(180deg)';
     } else {
-        toggleButton.textContent = 'Показать все';
+        toggleButton.querySelector('span').nextSibling.textContent = 'Показать все';
         additionalBrands.classList.add('brands__readnext__list_hidden');
+        arrowIcon.style.transform = 'rotate(0deg)';
     }
 });
